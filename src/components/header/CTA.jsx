6a93c529cf582991d695ff51a7a45fd33552e683
Sunil Reddy <sunilreddy.chinthaparti.com>
2022-10-06 21:46:25 +0530

    fix the boolian error

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/cv.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} dounload className="btn">
+      <a href={CV} className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
