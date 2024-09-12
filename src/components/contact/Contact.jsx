cefe03c4403403ae57c7cceeda6222c37ffbdca4
Sunil Chinthapathi <sunilchinthaparti@gmail.com>
2024-09-12 17:59:07 -0400

    corrected contact spelling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -22,7 +22,7 @@ function Contact() {
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
-      <h2>Cinatct Me </h2>
+      <h2>Contact Me </h2>
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
